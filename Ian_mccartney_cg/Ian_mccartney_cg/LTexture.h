d96a7d8a895cb2717e85d618ead772455e0bd510
Cpt.Cobra(Mobile) <Cpt.Cobra(Mobile)>
2016-06-02 20:04:13 -0700

    Working on Ltextures

---
+++ Ian_mccartney_cg/Ian_mccartney_cg/LTexture.h
@@ -0,0 +1,48 @@
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+#pragma once
+
+//Texture wrapper class
+class LTexture
+{
+public:
+	//Initializes variables
+	LTexture();
+
+	//Deallocates memory
+	~LTexture();
+
+	//Loads image at specified path
+	bool LoadFromFile(std::string path);
+
+	//Deallocates texture
+	void Free();
+
+	//Set color modulation
+	void SetColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	//Set blending
+	void SetBlendMode(SDL_BlendMode blending);
+
+	//Set alpha modulation
+	void SetAlpha(Uint8 alpha);
+
+	//Renders texture at given point
+	void Render(int x, int y, SDL_Rect* clip = NULL);
+
+	//Gets image dimensions
+	int GetWidth();
+	int GetHeight();
+
+private:
+	//The actual hardware texture
+	SDL_Texture* mTexture;
+
+	//Image dimensions
+	int mWidth;
+	int mHeight;
+};
